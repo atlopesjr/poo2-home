92e6abe2ff5c9b2a462ffe71666c7c5db4b7dcdf
unknown <atlopesjunior@gmail.com>
2020-03-28 22:54:21 -0300

    poo2-presenca23032020

--- src/main/java/com/example/poo2presenca23032020/repository/CarRepository.java
+++ src/main/java/com/example/poo2presenca23032020/repository/CarRepository.java
@@ -14,11 +14,11 @@ public class CarRepository {
     public HashMap<Integer,Car> hm = new HashMap<>();
     
     public CarRepository(){
-        hm.put(1, new Car(1, "Volvo", "XC 60", "Branca", 250.000));
-        hm.put(2, new Car(2, "Volvo", "XC 60", "Chumbo", 255.000));
-        hm.put(3, new Car(3, "Honda", "Civic", "Branca", 65.000));
-        hm.put(4, new Car(4, "Ford", "Focus", "Branca", 60.000));
-        hm.put(5, new Car(5, "Fiat", "Uno", "Branca", 50.000));
+        hm.put(1, new Car(1, "Volvo", "XC 60", "Branca", 250000));
+        hm.put(2, new Car(2, "Volvo", "XC 60", "Chumbo", 255000));
+        hm.put(3, new Car(3, "Honda", "Civic", "Branca", 65000));
+        hm.put(4, new Car(4, "Ford", "Focus", "Branca", 60000));
+        hm.put(5, new Car(5, "Fiat", "Uno", "Branca", 50000));
     }
 
     public Car getCarById(int id){
